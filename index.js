5d973960e1c831ba9963846f613f727102ee8530
Bruce Bai <brbai@ucsc.edu>
2023-05-23 12:21:55 -0700

    finalize adding

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-=======
-
->>>>>>> 9104b7577bb015cd40937277146fd4fd4ab38aed
