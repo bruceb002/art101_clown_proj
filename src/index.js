6796c6802ffb20ae64f683ba9a750e0ca3437a06
Bruce Bai <brbai@ucsc.edu>
2023-05-27 03:59:24 -0700

    lets see

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import { createRoot } from 'react-dom/client';
 const domNode = document.getElementById('dress_up_page');
 const root = createRoot(domNode);
 
-root.render(<h1>Hello World!</h1>);
+root.render(<App />);
