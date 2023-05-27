7d0fe4cbbc4bdc73450728915985ae963d924849
Bruce Bai <brbai@ucsc.edu>
2023-05-26 18:43:37 -0700

    logging

--- site.js
+++ site.js
@@ -3,7 +3,9 @@ function toggle_sites() {
     var title = document.getElementById("title_page");
     var dress_up = document.getElementById("dress_up_page");
     var show = title.style.display;
+    console.log("This is show: " + show + "\n");
     var hide = dress_up.style.display;
+    console.log("This is hide: " + hide + "\n");
     title.style.display = hide;
     dress_up.style.display = show;
     
