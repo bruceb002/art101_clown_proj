0f9e0802838bac726c7d0d244cf0fec6e7592382
Bruce Bai <brbai@ucsc.edu>
2023-05-26 17:53:29 -0700

    testing more

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import React from './typescript/react';
 import ReactDOM from './typescript/react-dom';
 import App from './Components/App.js';
 
-ReactDOM.render(<App/>, document.getElementById('dress_up_page'));
+ReactDOM.render(<App />, document.getElementById('dress_up_page'));
