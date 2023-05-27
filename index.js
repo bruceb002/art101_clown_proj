17930c97056d982185ce9ae5b0af21613f0f87d6
Bruce Bai <brbai@ucsc.edu>
2023-05-27 01:07:51 -0700

    testing

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import App from './Components/App'
+import App from './Components/App.js'
 import { createRoot } from 'react-dom/client';
 
 const domNode = document.getElementById('dress_up_page');
 const root = createRoot(domNode);
 
-root.render(<p>Hello World!</p>);
+root.render(<h1>Hello World!</h1>);
