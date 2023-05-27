b370576119b9b3bf5a55c7136899460c16690f6f
Bruce Bai <brbai@ucsc.edu>
2023-05-26 19:04:11 -0700

    reverted back to og method

--- site.js
+++ site.js
@@ -2,11 +2,8 @@
 function toggle_sites() {
     var title = document.getElementById("title_page");
     var dress_up = document.getElementById("dress_up_page");
-    var show = title.style.display;
-    console.log("This is show: ", title.style.display.toString());
-    var hide = dress_up.style.display;
-    title.style.display = hide;
-    dress_up.style.display = show;
+    title.style.display = "none";
+    dress_up.style.display = "block";
     
     /* Should we add music? */
 }
