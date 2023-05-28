1f30664fa7ec3ff94218d185927447267806e683
Bruce Bai <brbai@ucsc.edu>
2023-05-27 18:07:03 -0700

    hm

--- build/src/Components/constants/ItemTypes.js
+++
@@ -1,5 +0,0 @@
-export const ItemTypes = {
-  HAIR_RED: 'hair_red',
-  HAIR_ORANGE: 'hair_orange',
-  HAIR_YELLOW: 'hair_yellow',
-  };
--- build/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import App from './Components/App'
-import { createRoot } from 'react-dom/client';
-
-const domNode = document.getElementById('dress_up_page');
-const root = createRoot(domNode);
-
-root.render(<App />);
