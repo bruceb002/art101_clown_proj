f73b729d6c7750007b572c42d5bc936dea0ca6b9
Bruce Bai <brbai@ucsc.edu>
2023-05-30 15:10:20 -0700

    Updates

--- site.js
+++ site.js
@@ -8,24 +8,62 @@ function toggle_sites() {
     /* Should we add music? */
 }
 
-function allowDrop(ev) {
-    ev.preventDefault();
-  }
+let draggedAccessory = null;
+
+function dragStart(event) {
+  draggedAccessory = this;
+  this.classList.add('dragging');
+  event.dataTransfer.setData('text/plain', event.target.id);
+}
+
+function dragEnd() {
+  this.classList.remove('dragging');
+}
+
+function dragOver(event) {
+  event.preventDefault();
+}
+
+function dragEnter(event) {
+  event.preventDefault();
+  this.classList.add('hovered');
+}
+
+function dragLeave() {
+  this.classList.remove('hovered');
+}
+
+function drop(event) {
+  this.classList.remove('hovered');
+  const droppedAccessory = document.getElementById(event.dataTransfer.getData('text/plain'));
   
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+  if (this.contains(droppedAccessory)) {
+    // Accessory is being dropped back to its original position
+    this.appendChild(droppedAccessory);
+  } else {
+    // Accessory is being dropped onto the clown container
+    const clonedAccessory = droppedAccessory.cloneNode(true);
+    this.appendChild(clonedAccessory);
   }
-  
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
+}
 
 function main() {
     $("#dress_up_page").css("display", "none");
     var button = document.getElementById("start");
     button.addEventListener("click", toggle_sites);
+
+    const accessories = document.querySelectorAll('.cushion1');
+    const clownContainer = document.getElementById('doll-base');
+
+    accessories.forEach(accessory => {
+      accessory.addEventListener('dragstart', dragStart);
+      accessory.addEventListener('dragend', dragEnd);
+    });
+
+    clownContainer.addEventListener('dragover', dragOver);
+    clownContainer.addEventListener('dragenter', dragEnter);
+    clownContainer.addEventListener('dragleave', dragLeave);
+    clownContainer.addEventListener('drop', drop);
 }
 
 main();
