7fd7aa72ba14c1aee7a654d9298c054bcdf363b8
Bruce Bai <brbai@ucsc.edu>
2023-06-01 14:28:34 -0700

    Updates

--- site.js
+++ site.js
@@ -55,8 +55,8 @@ function main() {
 
     //handle drag and drop
     var accessories = document.querySelectorAll('.cushion1');
-    var clownContainer = $("#clownZone");
-    console.log("This is clownContainer: ", clownContainer);
+    var clownContainer = document.getElementById("doll-container");
+    console.log("This is clownContainer: ", document.getElementById("doll-container"));
 
     accessories.forEach(accessory => {
       accessory.addEventListener('dragstart', dragStart);
