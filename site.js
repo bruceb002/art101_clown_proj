eb6c9e0afbfd09e23ffd28608f1c67a99b77ebb0
Bruce Bai <brbai@ucsc.edu>
2023-06-03 19:57:15 -0700

    Updates

--- site.js
+++ site.js
@@ -14,9 +14,25 @@ function main() {
     $("#start").click(toggle_sites);
 
     //handle drag and drop
-    $("#accessory").draggable();
-    $("#doll-container").droppable({accept: "#accessory"});
+    $(".accessory").draggable();
+    $("#doll-container").droppable({
+        accept: ".accessory",
+        drop: (_, ui) => {
+            //get id (including the hashtag) of accessory
+            var dragged_item_id = "#" + ui.draggable.attr("id");
+            console.log(dragged_item_id);
 
+            //add accesory to doll body
+            $(dragged_item_id).appendTo("#doll-container");
+            $("#doll-container").css("position", "relative");
+            var new_placement = "#doll-container" + dragged_item_id;
+            $(new_placement).css("position", "absolute");
+
+            //hide display of original position of element
+            var original_placement = ".cushion" + dragged_item_id;
+            $(original_placement).hide();
+        }
+    });
 }
 
 main();
