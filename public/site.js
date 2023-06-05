56de70b53a92ed5e6c914b508cbba1653ba2bbdf
Bruce Bai <brbai@ucsc.edu>
2023-06-05 15:21:16 -0700

    added audio

--- public/site.js
+++ public/site.js
@@ -1,11 +1,23 @@
+function audio_control() {
+    var aud = document.getElementById("game-audio");
+    aud.volume = 0.5;
+    aud.play();
+    if (promise !== undefined) {
+        promise.then(_ => {
+          console.log("Autoplay started!");
+        }).catch(error => {
+            console.log("Autoplay was prevented. Show a 'Play' button so that user can start playback.")
+        });
+      }
+}
+
 function toggle_sites() {
     // Toggle visibility of the two pages using jQuery
     var titlePage = $("#title_page");
     var dressUpPage = $("#dress_up_page");
     titlePage.css("display", "none");
     dressUpPage.css("display", "block");
-
-    /* Should we add music? */
+    audio_control();
 }
 
 function main() {
