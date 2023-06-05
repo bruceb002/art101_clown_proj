fcccf3be77d419b40ed3697f73c88b715a06f92d
Bruce Bai <brbai@ucsc.edu>
2023-06-05 15:21:05 -0700

    Updates

--- site.js
+++ site.js
@@ -2,6 +2,13 @@ function audio_control() {
     var aud = document.getElementById("game-audio");
     aud.volume = 0.5;
     aud.play();
+    if (promise !== undefined) {
+        promise.then(_ => {
+          console.log("Autoplay started!");
+        }).catch(error => {
+            console.log("Autoplay was prevented. Show a 'Play' button so that user can start playback.")
+        });
+      }
 }
 
 function toggle_sites() {
