029d82363b891122c114463009369b24214e0d2b
Bruce Bai <brbai@ucsc.edu>
2023-06-05 15:02:40 -0700

    Updates

--- site.js
+++ site.js
@@ -1,11 +1,16 @@
+function audio_control() {
+    var audio = document.getElementById("game-audio");
+    audio.volume = 0.5;
+    audio.play();
+}
+
 function toggle_sites() {
     // Toggle visibility of the two pages using jQuery
     var titlePage = $("#title_page");
     var dressUpPage = $("#dress_up_page");
     titlePage.css("display", "none");
     dressUpPage.css("display", "block");
-
-    /* Should we add music? */
+    audio_control();
 }
 
 function main() {
