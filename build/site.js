f53b3770b5c2f6e6ca09b2c8addd03616c03cf00
Bruce Bai <brbai@ucsc.edu>
2023-06-06 01:17:11 -0700

    hm

---
+++ build/site.js
@@ -0,0 +1,25 @@
+function audio_control() {
+    var aud = document.getElementById("game-audio");
+    aud.volume = 0.5;
+    aud.play();
+}
+
+function toggle_sites() {
+    // Toggle visibility of the two pages using jQuery
+    var titlePage = $("#title_page");
+    var dressUpPage = $("#dress_up_page");
+    titlePage.css("display", "none");
+    dressUpPage.css("display", "block");
+    $("html").css('background-color', '#fff0dd');
+    
+    //play audio
+    audio_control();
+}
+
+function main() {
+    //go from title to dress up screen
+    $("#dress_up_page").css("display", "none");
+    $("#start").click(toggle_sites);
+}
+
+main();
