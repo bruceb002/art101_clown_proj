aaad261ef3a57a23059a0631fc1457ce4edf31c7
Bruce Bai <brbai@ucsc.edu>
2023-06-07 00:42:46 -0700

    Updates

--- site.js
+++ site.js
@@ -8,8 +8,13 @@ function toggle_sites() {
     // Toggle visibility of the two pages using jQuery
     var titlePage = $("#title_page");
     var dressUpPage = $("#dress_up_page");
+
     titlePage.css("display", "none");
     dressUpPage.css("display", "block");
+
+    $("html").css('background-position', 'center');
+    $("html").css('background-size', 'cover');
+    $("html").css('background-repeat', 'no-repeat');
     $("html").css("background-image", "url('./img/dress-up-background.jpeg')");
     
     //play audio
